c12cc12d1ec608ca08e353223338761d5478ffad
xinyuCai <2876626746@qq.com>
2017-07-06 11:16:53 +0800

    我的第四天美团代码提交

---
+++ meituan/js/index.js
@@ -0,0 +1,9 @@
+/*文档就绪函数*/
+$(function(){
+    //click比tap慢300ms
+    //$(this)[0]: $(this)转换为this（jQuery对象转换为原生对象）
+    //$(this):    this转换为$(this)(原生对象转换为jQuery对象)
+    $("#nav span").on("tap",function(){
+
+    });
+});
