c1d4f4515849e03221343ec37ea2bbd66ad6dc23
xinyuCai <2876626746@qq.com>
2017-07-10 11:31:36 +0800

    美团2017/7/10 commit1

--- meituan/js/index.js
+++ meituan/js/index.js
@@ -1,50 +1,56 @@
 /*文档就绪函数*/
-$(function(){
-    //click比tap慢300ms
-    //$(this)[0]: $(this)转换为this（jQuery对象转换为原生对象）
-    //$(this):    this转换为$(this)(原生对象转换为jQuery对象)
-    var $navSpan = $("#nav span");
-    var $guideUl=$("#guide ul");
-    function swipeGuide(index){
-        $navSpan.eq(index).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: -index * $guideUl.width() / 2
-        });
-    }
-    $navSpan.on("tap",function(){
-        swipeGuide($(this).index());
-    });
-    $guideUl.on("swipeleft",function(){
-        swipeGuide(1);
-    }).on("swiperight",function(){
-        swipeGuide(0)
-    });
-
-    var bShow = false;
-    $(document).on('scroll', function(){
-        var scrollTop = document.documentElement.scrollTop
-            || document.body.scrollTop;
-        if(scrollTop >= 200 && bShow == false){
-            /*$('#go-top').fadeIn();*/
-            $('#go-top').css("display","block");
-            bShow = true;
-            console.log("显示");
-        }else if(scrollTop < 200 && bShow == true){
-            /*$('#go-top').fadeOut();*/
-            $('#go-top').css("display","none");
-            bShow = false;
-            console.log("消失");
+$(document).on("pageinit",function(event){
+    $(function(){
+        //click比tap慢300ms
+        //$(this)[0]: $(this)转换为this（jQuery对象转换为原生对象）
+        //$(this):    this转换为$(this)(原生对象转换为jQuery对象)
+        var $navSpan = $("#nav span");
+        var $guideUl=$("#guide ul");
+        function swipeGuide(index){
+            $navSpan.eq(index).addClass("active").siblings().removeClass("active");
+            $guideUl.css({
+                left: -index * $guideUl.width() / 2
+            });
         }
-    });
-    $('#go-top').on('tap', function(){
-        var bottom = $(this).css("bottom");
-        $(this).animate({
-            top: 0
-        }, 500, function(){
-            $(this).css("bottom", bottom);
+        $navSpan.on("tap",function(){
+            swipeGuide($(this).index());
         });
-        $('body').animate({
-            scrollTop: 0
-        },500);
+        $guideUl.on("swipeleft",function(){
+            swipeGuide(1);
+        }).on("swiperight",function(){
+            swipeGuide(0)
+        });
+
+        var bShow = false;
+        $(document).on('scroll', function(){
+            var scrollTop = document.documentElement.scrollTop
+                || document.body.scrollTop;
+            if(scrollTop >= 200 && bShow == false){
+                $('#go-top').fadeIn();
+                /*$('#go-top').css("display","block");*/
+                bShow = true;
+                console.log("显示");
+            }else if(scrollTop < 200 && bShow == true){
+                $('#go-top').fadeOut();
+                /*$('#go-top').css("display","none");*/
+                bShow = false;
+                console.log("消失");
+            }
+        });
+        $('#go-top').on('tap', function(){
+            /*var bottom = $(this).css("bottom");
+             $(this).animate({
+             top: 0
+             }, 500, function(){
+             $(this).css("bottom", bottom);
+             });*/
+            $('body').animate({
+                scrollTop: 0
+            },500);
+        });
+
+        $("#guess #menu li").on("tap",function(){
+            location.href = "detail.html";
+        });
     });
 });
